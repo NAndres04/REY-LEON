b0593aca23181bb7ae518f3491e369b7831b2f75
jeancitoh <109388818+jeancitoh@users.noreply.github.com>
2022-11-07 01:25:37 -0300

    ortografia

--- resources/views/directora/posteos/VerPost.blade.php
+++ resources/views/directora/posteos/VerPost.blade.php
@@ -15,7 +15,7 @@
     
     <img src="<?php echo $actividad->imagen ?>" />
     <span>{{ $actividad->descripcion }}</span>
-    <button type="submit" class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Ver publicacion</button>
+    <button type="submit" class="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Ver publicación</button>
     </form>
    @endforeach
 
